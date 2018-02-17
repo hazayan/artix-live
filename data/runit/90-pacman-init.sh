9c6b20fea3c485364c0d55039b017e3529662138
Muhammad Herdiansyah <koni@artixlinux.org>
2018-02-17 12:00:58 +0700

    Fix typo

--- data/runit/90-pacman-init.sh
+++ data/runit/90-pacman-init.sh
@@ -3,4 +3,4 @@ mount -t tmpfs -o size=10M,mode=0755 tmpfs /etc/pacman.d/gnupg
 
 msg "Initializing pacman"
 pacman-key --init
-pacman-key --populate archlinux-artix
+pacman-key --populate archlinux artix
